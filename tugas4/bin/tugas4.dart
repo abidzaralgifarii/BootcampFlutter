b75ad5cdc967409799aaf69d2ea525b66dccbb14
abidzar10 <11220657@nusamandiri.ac.id>
2024-08-22 21:46:21 +0700

    hapus

--- tugas4/bin/tugas4.dart
+++
@@ -1,5 +0,0 @@
-import 'package:tugas4/tugas4.dart' as tugas4;
-
-void main(List<String> arguments) {
-  print('Hello world: ${tugas4.calculate()}!');
-}
--- tugas4/test/tugas4_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:tugas4/tugas4.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
